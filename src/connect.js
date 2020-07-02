ce4597f2eba5fab5a332eec40b4d455f22dfba36
Junior Miksza <junior.miksza8@gmail.com>
2020-07-02 19:02:49 -0300

    Final teste

--- src/connect.js
+++ src/connect.js
@@ -1,16 +1,24 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-var url;
 
-if(process.env.NODE_ENV  == 'test '){
-  url = 'mongodb://127.0.0.1:27017/testdev?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false';
-}
+
 
 try {
-  mongoose.connect(process.env.DB_MAIN, {
-    useNewUrlParser: true,
-    useUnifiedTopology: process.env.NODE_ENV === 'test ' ? false : true ,
-  }); 
+  
+
+  if(process.env.NODE_ENV  == 'test '){
+    var url = 'mongodb://127.0.0.1:27017/testdev?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false';
+    mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: process.env.NODE_ENV === 'test ' ? false : true ,
+    }); 
+  }else{
+    mongoose.connect(process.env.DB_MAIN, {
+      useNewUrlParser: true,
+      useUnifiedTopology: process.env.NODE_ENV === 'test ' ? false : true ,
+    }); 
+  }
+  
 
   mongoose.set('useFindAndModify', false);
   mongoose.set('useCreateIndex', true);
@@ -23,7 +31,5 @@ try {
 mongoose.connection.on('error', (error) => {
   console.error(error);
 });
-
-mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
